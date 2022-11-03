92a70ed253e4e2dc964a835619388c0d578b0fea
Tauhidul <tauhidulhossaintanmoy@gmail.com>
2022-11-04 00:21:02 +0600

    Raw Files

---
+++ index.js
@@ -0,0 +1,12 @@
+const _ = require('lodash');
+const people = require('./test/people');
+
+console.log(_.last(people));
+
+// setTimeout(() => {
+//     console.log('test');
+// }, 1000);
+console.log(global);
+console.log(__dirname);
+console.log(__dirname);
+console.log(__filename);
---
+++ test/people.js
@@ -0,0 +1,2 @@
+const people = ['a', 'b', 'c', 'd', 'e'];
+module.exports = people;
