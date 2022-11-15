a3b6e8718ca2286b061367eace875ce8fa2800cd
Tauhidul <tauhidulhossaintanmoy@gmail.com>
2022-11-15 23:35:09 +0600

    todo App

--- Js Project/app.js
+++ Js Project/app.js
@@ -1,3 +1,20 @@
-// const element = document.createElement('div');
-// element.innerText = todo1;
-// document.body.appendChild(element);
+// let count = 0;
+
+// function countUp() {
+//     count += 1;
+//     upDateCount();
+// }
+// function countDown() {
+//     count -= 1;
+//     upDateCount();
+// }
+
+// function upDateCount() {
+//     let counter = document.getElementById('counter');
+//     counter.innerText = count;
+// }
+
+// function buttonTextAfterClick() {
+//     let button = document.getElementById('add-new-item');
+//     button.innerText = 'Done';
+// }
